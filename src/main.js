10b7e5310a992b499cfce3fe1694d099e8fb7440
Guard84 <guardsofword@gmail.com>
2024-09-04 16:22:48 +0300

    upd styles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+const gallery = document.querySelector('.reviews-track');
+let cardIndex = 0;
 
+function autoScroll() {
+  const cards = document.querySelectorAll('.reviews-card');
+  cardIndex++;
+
+  if (cardIndex >= cards.length) {
+    cardIndex = 0;
+  }
+
+  gallery.style.transform = `translateX(-${cardIndex * 100}%)`;
+}
+
+setInterval(autoScroll, 4000);
